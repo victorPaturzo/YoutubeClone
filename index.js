878860cf0b86a6a38aec5607587a5ff58c7ebafd
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2021-11-28 10:22:21 -0500

    Created Post for comment to video and replies to comments, and Put for likes/dislikes. (VPP, GD)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const comment = require('./routes/comment')
 connectDB();
 app.use(express.json());
 app.use('/api/comment', comment);
-const port =process.env.PORT ||5000;
+const port = process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`Server started on port: ${port}`);
 });
--- routes/comment.js
+++ routes/comment.js
@@ -1,3 +1,4 @@
+const { productSchema } = require('../../../Assignments_UserStories/MongooseTutorial/models/product');
 const{Reply, validate}=require('../models/comment');
 const{Comment, validate}=require('../models/comment');
 const router=express.Router();
@@ -11,5 +12,81 @@ router.get('/', async (req,res)=>{
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error)
+            return res.status(400).send(error);
 
+        const comment = new Comment({
+            videoId: req.body.videoId,
+            text: req.body.text,
+            likes: req.body.likes,
+            dislikes: req.body.dislikes,
+            replies: req.body.replies,
+        });
+
+        await comment.save();
+
+        return res.send(comment);
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`)
+    }
+});
+
+router.put('/', async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send(error);
+
+        const comment = await comment.findByIdAndUpdate(
+            req.params.id,
+            {
+                likes: req.body.likes,
+                dislikes: req.body.dislikes,
+            },
+            { new: true}
+        );
+        
+        if (!comment)
+            return res.status(400).send(`The comment requested does not exist.`)
+
+        await comment.save();
+
+        return res.send(comment);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`)
+    }
+});
+
+router.post('/:id', async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send(error);
+
+        const comment = await Comment.findByIdAndUpdate(
+            req.params.id,
+            {
+               text: req.body.text,
+               likes: req.body.likes,
+               dislikes: req.body.dislikes, 
+            },
+            { new: true }
+        );
+
+        if (!comment)
+            return res.status(400).send(`The comment requested does not exist.`);
+
+        await comment.save();
+
+        return res.send(comment);
+    } catch (ex)  {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+
+//make a put request to backend api to like or dislike a comment
+
+//make a post request to backend api toreply to another comment in the comment section
 module.exports=router;
